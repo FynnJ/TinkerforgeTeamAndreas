c33ae8f6c6c49a530ded69844af0d4a099d09dc2
JansenF <fynnjoeljansen@gmail.com>
2025-05-01 15:27:08 +0200

    add di for ipconnection

---
+++ Tinkerforge.Worker/Notifier/NotifierService.cs
@@ -0,0 +1,14 @@
+﻿namespace Tinkerforge.Worker.Notifier;
+
+public class NotifierService(ILogger<NotifierService> logger, IPConnection ipConnection) : IFeatureService
+{
+    private const string Uid = "Wcg";
+
+    public void ExecuteService()
+    {
+        var ptc = new BrickletPTCV2(Uid, ipConnection);
+
+        // var temperature = ptc.GetTemperature();
+        logger.LogInformation("Temperature: {Temperature} °C", 3000 / 100);
+    }
+}
--- Tinkerforge.Worker/Worker.cs
+++ Tinkerforge.Worker/Worker.cs
@@ -1,27 +1,22 @@
+using Tinkerforge.Worker.Notifier;
+
 namespace Tinkerforge.Worker;
 
-public class Worker(ILogger<Worker> logger) : BackgroundService
+public class Worker(
+    ILogger<Worker> logger,
+    IPConnection ipconnection,
+    NotifierService notifierService)
+    : BackgroundService
 {
-    private const string Host = "172.20.10.242";
-    private const int Port = 4223;
-    private readonly IPConnection _ipConnection = new();
-
-    private const string Uid = "Wcg";
-
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         try
         {
-            _ipConnection.Connect(Host, Port);
-            var ptc = new BrickletPTCV2(Uid, _ipConnection);
-
             while (!stoppingToken.IsCancellationRequested)
             {
+                notifierService.ExecuteService();
+
                 logger.LogInformation("Worker running at: {Time}", DateTimeOffset.Now);
-
-                var temperature = ptc.GetTemperature();
-                logger.LogInformation("Temperature: {Temperature} °C", temperature / 100);
-
                 await Task.Delay(1000, stoppingToken);
             }
         }
@@ -31,8 +26,8 @@ public class Worker(ILogger<Worker> logger) : BackgroundService
         }
         finally
         {
-            _ipConnection.Disconnect();
-            logger.LogInformation("Worker stopped successfully");
+            // ipconnection.Disconnect();
+            logger.LogInformation("Successfully disconnected from Tinkerforge");
         }
     }
 }
