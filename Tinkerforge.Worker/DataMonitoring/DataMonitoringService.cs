755d6c36cfce10c05a349af3f2204cf94f380449
Lars-1008 <lars.lambrecht@osnanet.de>
2025-05-12 12:40:31 +0200

    Der Haifisch wird vom Vogel gefresse

--- Tinkerforge.Worker/DataMonitoring/DataMonitoringService.cs
+++ Tinkerforge.Worker/DataMonitoring/DataMonitoringService.cs
@@ -1,40 +1,87 @@
 namespace Tinkerforge.Worker.DataMonitoring;
 
-using System;
 using Microsoft.Extensions.Logging;
 using Tinkerforge;
+using System.Threading;
 
-public class DataMonitoringService(ILogger<DataMonitoringService> logger, IPConnection ipConnection) : IFeatureService
+public class DataMonitoringService(IPConnection ipConnection) : IFeatureService
 {
-    private static string DisplayUID = "Tre";
+    private static string DisplayUID = "24Rh";
     private static string TempUID = "Wcg";
+    private static string HumidUID = "ViW";
+    private static string LightUID = "Pdw";
+    private static string ButtonUID = "Vg8";
+
+    private int currentValue = 0;
 
     public void ExecuteService()
     {
-        var display = new BrickletSegmentDisplay4x7V2(DisplayUID, ipConnection);
-        display.SetBrightness(7);
-
+        var lcd = new BrickletLCD128x64(DisplayUID, ipConnection);
         var ptc = new BrickletPTCV2(TempUID, ipConnection);
-        int tempRaw = ptc.GetTemperature(); 
-        double temperature = tempRaw / 100.0;
+        var humid = new BrickletHumidityV2(HumidUID, ipConnection);
+        var light = new BrickletAmbientLightV3(LightUID, ipConnection);
+        var dualButton = new BrickletDualButtonV2(ButtonUID, ipConnection);
 
-        int roundedTemp = (int)Math.Round(temperature);
-        short[] values = ConvertToNumericArray(roundedTemp);
-        display.SetNumericValue(values);
+        // LCD Setup
+        lcd.SetDisplayConfiguration(128, 255, false, true);
 
-        logger.LogInformation($"Aktuelle Temperatur: {temperature} °C");
+        DisplayCurrentValue(lcd, ptc, humid, light);
+
+        while (true)
+        {
+            byte buttonL, buttonR;
+            dualButton.GetButtonState(out buttonL, out buttonR);
+
+            if (buttonL == 0) 
+            {
+                Thread.Sleep(20);
+                SwitchDisplayValue(reverse: true); 
+                DisplayCurrentValue(lcd, ptc, humid, light);
+            }
+            else if (buttonR == 0)
+            {
+                Thread.Sleep(20);
+                SwitchDisplayValue(reverse: false);
+                DisplayCurrentValue(lcd, ptc, humid, light);
+            }
+
+            Thread.Sleep(1);
+        }
     }
 
-    private short[] ConvertToNumericArray(int value)
+    private void DisplayCurrentValue(BrickletLCD128x64 lcd, BrickletPTCV2 ptc, BrickletHumidityV2 humid, BrickletAmbientLightV3 light)
     {
-        string strValue = value.ToString().PadLeft(4);
-        short[] result = new short[4];
+        double temperature = ptc.GetTemperature() / 100.0;
+        double humidity = humid.GetHumidity() / 100.0;
+        double lightLevel = light.GetIlluminance() / 100.0;
 
-        for (int i = 0; i < 4; i++)
+        lcd.ClearDisplay();
+
+        switch (currentValue)
         {
-            result[i] = strValue[i] == ' ' ? (short)-1 : short.Parse(strValue[i].ToString());
+            case 0:
+                lcd.WriteLine(0, 0, $"Temp: {temperature:F1} °C");
+                break;
+            case 1:
+                lcd.WriteLine(0, 0, $"Humidity: {humidity:F1} %");
+                break;
+            case 2:
+                lcd.WriteLine(0, 0, $"Light: {lightLevel:F1} Lux");
+                break;
         }
 
-        return result;
+        lcd.SetDisplayConfiguration(128, 255, false, true);
+    }
+
+    private void SwitchDisplayValue(bool reverse)
+    {
+        if (reverse)
+        {
+            currentValue = (currentValue == 0) ? 2 : currentValue - 1;
+        }
+        else
+        {
+            currentValue = (currentValue + 1) % 3;
+        }
     }
 }
