23a21b30ecac90fa56beadf93399b965cf40d3d6
Zeilan <lerke.andreas.main@gmail.com>
2025-05-14 12:04:45 +0200

    Fixed Bugs

--- Tinkerforge.Worker/MotionSensor/MotionService.cs
+++ Tinkerforge.Worker/MotionSensor/MotionService.cs
@@ -5,17 +5,19 @@ namespace Tinkerforge.Worker.MotionSensor;
 public class MotionService (ILogger<MotionService> logger, IPConnection ipConnection, ITelegramService telegramService) : IFeatureService
 {
     private const string Uid = "ML4";
+    private BrickletMotionDetectorV2 MotionsensorObj;
     
     public void ExecuteService()
     {
-        BrickletMotionDetectorV2 MotionSensor = new BrickletMotionDetectorV2(Uid, ipConnection);
-        MotionSensor.SetSensitivity(100);
-        MotionSensor.MotionDetectedCallback += MotionDetected;
+        MotionsensorObj = new BrickletMotionDetectorV2(Uid, ipConnection);
+        MotionsensorObj.SetSensitivity(100);
+        MotionsensorObj.MotionDetectedCallback += MotionDetected;
     }
     void MotionDetected(BrickletMotionDetectorV2 sender)
     {
         sender.SetIndicator(255,255,255);
         StartCountdown(sender);
+        sender.MotionDetectedCallback -= MotionDetected;
     }
 
     void StartCountdown(BrickletMotionDetectorV2 sender)
@@ -23,7 +25,7 @@ public class MotionService (ILogger<MotionService> logger, IPConnection ipConnec
     const string Uid = "Tre";
 
         BrickletSegmentDisplay4x7V2 SegmentDisplay = new BrickletSegmentDisplay4x7V2(Uid, ipConnection);
-        SegmentDisplay.StartCounter(60,0,-1,1000);
+        SegmentDisplay.StartCounter(5,0,-1,1000);
         SegmentDisplay.CounterFinishedCallback += NotAuthorizedAlarm;
         sender.SetIndicator(0,0,0);
     }
@@ -35,5 +37,7 @@ public class MotionService (ILogger<MotionService> logger, IPConnection ipConnec
         BrickletPiezoSpeakerV2 Alarm = new BrickletPiezoSpeakerV2(Uid, ipConnection);
         Alarm.SetAlarm(800,2000,10,1,1,10000);
         telegramService.SendMessageAsync("Unautorisierte Bewegung erkannt");
+        sender.CounterFinishedCallback -= NotAuthorizedAlarm;
+        MotionsensorObj.MotionDetectedCallback += MotionDetected;
     }
 }
