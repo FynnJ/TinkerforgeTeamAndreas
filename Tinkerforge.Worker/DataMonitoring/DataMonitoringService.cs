5a2741e27d6e1a76143986c1bc725afedb4a7365
Lars-1008 <lars.lambrecht@osnanet.de>
2025-05-14 12:02:11 +0200

    "Wenn der Kühlschrank flüstert, hört man besser zu" - sagte ein alter weiser Mann an einem heißen Sommerabend in Silicon Valley zu Elon Musk

--- Tinkerforge.Worker/DataMonitoring/DataMonitoringService.cs
+++ Tinkerforge.Worker/DataMonitoring/DataMonitoringService.cs
@@ -1,30 +1,30 @@
 namespace Tinkerforge.Worker.DataMonitoring;
 
-using Microsoft.Extensions.Logging;
 using Tinkerforge;
 using System.Threading;
 
 public class DataMonitoringService(IPConnection ipConnection) : IFeatureService
 {
-    private static string DisplayUID = "24Rh";
-    private static string TempUID = "Wcg";
-    private static string HumidUID = "ViW";
-    private static string LightUID = "Pdw";
-    private static string ButtonUID = "Vg8";
+    private readonly string _displayId = "24Rh";
+    private readonly string _tempId = "Wcg";
+    private readonly string _humidId = "ViW";
+    private readonly string _lightId = "Pdw";
+    private readonly string _buttonId = "Vd8";
 
-    private int currentValue = 0;
+    private int _currentValue = 0;
 
     public void ExecuteService()
     {
-        var lcd = new BrickletLCD128x64(DisplayUID, ipConnection);
-        var ptc = new BrickletPTCV2(TempUID, ipConnection);
-        var humid = new BrickletHumidityV2(HumidUID, ipConnection);
-        var light = new BrickletAmbientLightV3(LightUID, ipConnection);
-        var dualButton = new BrickletDualButtonV2(ButtonUID, ipConnection);
+        var lcd = new BrickletLCD128x64(_displayId, ipConnection);
+        var ptc = new BrickletPTCV2(_tempId, ipConnection);
+        var humid = new BrickletHumidityV2(_humidId, ipConnection);
+        var light = new BrickletAmbientLightV3(_lightId, ipConnection);
+        var dualButton = new BrickletDualButtonV2(_buttonId, ipConnection);
 
-        // LCD Setup
+        dualButton.SetLEDState(BrickletDualButtonV2.LED_LEFT, BrickletDualButtonV2.LED_STATE_OFF);
+        dualButton.SetLEDState(BrickletDualButtonV2.LED_RIGHT, BrickletDualButtonV2.LED_STATE_OFF);
+
         lcd.SetDisplayConfiguration(128, 255, false, true);
-
         DisplayCurrentValue(lcd, ptc, humid, light);
 
         while (true)
@@ -34,39 +34,45 @@ public class DataMonitoringService(IPConnection ipConnection) : IFeatureService
 
             if (buttonL == 0) 
             {
-                Thread.Sleep(20);
-                SwitchDisplayValue(reverse: true); 
+                dualButton.SetLEDState(BrickletDualButtonV2.LED_LEFT, BrickletDualButtonV2.LED_STATE_ON);
+                Thread.Sleep(200); 
+                dualButton.SetLEDState(BrickletDualButtonV2.LED_LEFT, BrickletDualButtonV2.LED_STATE_OFF);
+
+                SwitchDisplayValue(reverse: true);
                 DisplayCurrentValue(lcd, ptc, humid, light);
             }
             else if (buttonR == 0)
             {
-                Thread.Sleep(20);
+                dualButton.SetLEDState(BrickletDualButtonV2.LED_RIGHT, BrickletDualButtonV2.LED_STATE_ON);
+                Thread.Sleep(200); 
+                dualButton.SetLEDState(BrickletDualButtonV2.LED_RIGHT, BrickletDualButtonV2.LED_STATE_OFF);
+
                 SwitchDisplayValue(reverse: false);
                 DisplayCurrentValue(lcd, ptc, humid, light);
             }
 
-            Thread.Sleep(1);
+            Thread.Sleep(10);
         }
     }
 
     private void DisplayCurrentValue(BrickletLCD128x64 lcd, BrickletPTCV2 ptc, BrickletHumidityV2 humid, BrickletAmbientLightV3 light)
     {
-        double temperature = ptc.GetTemperature() / 100.0;
-        double humidity = humid.GetHumidity() / 100.0;
-        double lightLevel = light.GetIlluminance() / 100.0;
+        var temperature = ptc.GetTemperature() / 100.0;
+        var humidity = humid.GetHumidity() / 100.0;
+        var lightLevel = light.GetIlluminance() / 100.0;
 
         lcd.ClearDisplay();
 
-        switch (currentValue)
+        switch (_currentValue)
         {
             case 0:
-                lcd.WriteLine(0, 0, $"Temp: {temperature:F1} °C");
+                lcd.WriteLine(0, 0, $"Temperatur: {temperature:F1} Celsius");
                 break;
             case 1:
-                lcd.WriteLine(0, 0, $"Humidity: {humidity:F1} %");
+                lcd.WriteLine(0, 0, $"Feuchtigkeit: {humidity:F1} %");
                 break;
             case 2:
-                lcd.WriteLine(0, 0, $"Light: {lightLevel:F1} Lux");
+                lcd.WriteLine(0, 0, $"Helligkeit: {lightLevel:F1} Lux");
                 break;
         }
 
@@ -77,11 +83,11 @@ public class DataMonitoringService(IPConnection ipConnection) : IFeatureService
     {
         if (reverse)
         {
-            currentValue = (currentValue == 0) ? 2 : currentValue - 1;
+            _currentValue = (_currentValue == 0) ? 2 : _currentValue - 1;
         }
         else
         {
-            currentValue = (currentValue + 1) % 3;
+            _currentValue = (_currentValue + 1) % 3;
         }
     }
 }
