af02f4d7020f623d1feae0b535e23e5131fb7f8d
Arrax <37862634+Vendaxa@users.noreply.github.com>
2025-05-28 12:10:58 +0200

    add authorization feature (#13)
    Co-authored-by: Nils Hengemühle <N.Hengemuehle@deos-ag.com>
    Co-authored-by: Fynn Joel Jansen <141991890+FynnJ@users.noreply.github.com>

--- Tinkerforge.Worker/Program.cs
+++ Tinkerforge.Worker/Program.cs
@@ -2,6 +2,7 @@ using Tinkerforge.Worker;
 using Tinkerforge.Worker.DataMonitoring;
 using Tinkerforge.Worker.MotionService;
 using Tinkerforge.Worker.NotificationService;
+using Tinkerforge.Worker.Authorization;
 
 var builder = Host.CreateApplicationBuilder(args);
 
@@ -9,6 +10,7 @@ builder.Services.AddServices();
 builder.Services.AddNotificationServices(builder.Configuration);
 builder.Services.AddMotionServices();
 builder.Services.AddDataMonitoringServices();
+builder.Services.AddAuthServices();
 
 var host = builder.Build();
 await host.RunAsync();
